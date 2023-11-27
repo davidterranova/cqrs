346b2ccee372353f5a4e3784d9f78994e3845d1e
david terranova <david@smallpdf.com>
2023-11-27 09:49:14 +0400

    remove usertype from user

--- user/user.go
+++ user/user.go
@@ -4,19 +4,10 @@ import (
 	"github.com/google/uuid"
 )
 
-type UserType string
-
-const (
-	UserTypeSystem          UserType = "system"
-	UserTypeAuthenticated   UserType = "authenticated"
-	UserTypeUnauthenticated UserType = "unauthenticated"
-)
-
 type UserFactory func() User
 
 type User interface {
 	Id() uuid.UUID
-	Type() UserType
 	String() string
 	FromString(string) error
 }
