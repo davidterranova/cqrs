c4de638eea9b76f2880788041cf71c3426056135
david terranova <david@smallpdf.com>
2023-12-01 20:04:07 +0400

    EventPublisher exponential backoff

--- eventsourcing/event_publisher.go
+++ eventsourcing/event_publisher.go
@@ -2,8 +2,10 @@ package eventsourcing
 
 import (
 	"context"
+	"errors"
 	"time"
 
+	"github.com/cenkalti/backoff/v4"
 	"github.com/rs/zerolog/log"
 )
 
@@ -13,45 +15,53 @@ type EventStreamPublisher[T Aggregate] struct {
 	eventRegistry EventRegistry[T]
 	userFactory   UserFactory
 	batchSize     int
+	backoff       bool
 }
 
-func NewEventStreamPublisher[T Aggregate](eventRepo EventRepository, eventRegistry EventRegistry[T], userFactory UserFactory, stream Publisher[T], batchSize int) *EventStreamPublisher[T] {
+func NewEventStreamPublisher[T Aggregate](eventRepo EventRepository, eventRegistry EventRegistry[T], userFactory UserFactory, stream Publisher[T], batchSize int, backoff bool) *EventStreamPublisher[T] {
 	return &EventStreamPublisher[T]{
 		eventRepo:     eventRepo,
 		eventRegistry: eventRegistry,
 		userFactory:   userFactory,
 		stream:        stream,
 		batchSize:     batchSize,
+		backoff:       backoff,
 	}
 }
 
 func (p *EventStreamPublisher[T]) Run(ctx context.Context) {
-	var (
-		nb  int
-		err error
-	)
+	var b backoff.BackOff
+	if !p.backoff {
+		b = backoff.NewConstantBackOff(0 * time.Millisecond)
+	} else {
+		b = backoff.WithMaxRetries(
+			backoff.WithContext(
+				backoff.NewExponentialBackOff(),
+				ctx,
+			),
+			5,
+		)
+	}
 
 	for {
 		select {
 		case <-ctx.Done():
 			return
 		default:
-			nb, err = p.processBatch(ctx)
-			if err != nil {
-				log.Ctx(ctx).Error().Err(err).Msg("event stream publisher: failed to process batch")
-			}
-		}
+			_ = backoff.Retry(func() error {
+				nb, err := p.processBatch(ctx)
+				if err != nil {
+					log.Ctx(ctx).Error().Err(err).Msg("event stream publisher: failed to process batch")
+					return err
+				}
 
-		var sleepTime time.Duration
-		switch nb {
-		case -1:
-			sleepTime = 1 * time.Second
-		case 0:
-			sleepTime = 1 * time.Second
-		default:
-			sleepTime = 10 * time.Millisecond
+				if nb == 0 {
+					return errors.New("no events to publish")
+				}
+
+				return nil
+			}, b)
 		}
-		time.Sleep(sleepTime)
 	}
 }
 
