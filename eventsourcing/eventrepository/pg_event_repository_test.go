2d79269c3086ce27a329e7aa1473b8db88c24d72
david terranova <david@smallpdf.com>
2023-12-02 17:09:49 +0400

    add integration tag for pg related tests

--- eventsourcing/eventrepository/pg_event_repository_test.go
+++ eventsourcing/eventrepository/pg_event_repository_test.go
@@ -1,3 +1,6 @@
+//go:build integration
+// +build integration
+
 package eventrepository_test
 
 import (
@@ -61,41 +64,3 @@ func testDB(t *testing.T) *gorm.DB {
 
 	return db
 }
-
-// func TestEventStore(t *testing.T) {
-// 	registry := eventsourcing.NewRegistry[domain.Contact]()
-// 	registry.Register(domain.ContactCreated, func() eventsourcing.Event[domain.Contact] { return &domain.EvtContactCreated{} })
-// 	registry.Register(domain.ContactNameUpdated, func() eventsourcing.Event[domain.Contact] { return &domain.EvtContactNameUpdated{} })
-// 	registry.Register(domain.ContactEmailUpdated, func() eventsourcing.Event[domain.Contact] { return &domain.EvtContactEmailUpdated{} })
-// 	registry.Register(domain.ContactPhoneUpdated, func() eventsourcing.Event[domain.Contact] { return &domain.EvtContactPhoneUpdated{} })
-// 	registry.Register(domain.ContactDeleted, func() eventsourcing.Event[domain.Contact] { return &domain.EvtContactDeleted{} })
-
-// 	db, err := pg.Open(pg.DBConfig{
-// 		Name:       "postgres",
-// 		ConnString: "postgres://postgres:password@127.0.0.1:5432/contacts?sslmode=disable&search_path=event_store",
-// 	})
-// 	require.NoError(t, err)
-// 	store := eventsourcing.NewPGEventStore(db, registry)
-
-// 	issuer := user.New(uuid.New())
-// 	contactId := uuid.New()
-
-// 	t.Run("Store", func(t *testing.T) {
-// 		events := []eventsourcing.Event[domain.Contact]{
-// 			domain.NewEvtContactCreated(contactId, issuer),
-// 			domain.NewEvtContactNameUpdated(contactId, issuer, "John", "Doe"),
-// 			domain.NewEvtContactEmailUpdated(contactId, issuer, "jdoe@contact.local"),
-// 			domain.NewEvtContactPhoneUpdated(contactId, issuer, "+33612345678"),
-// 		}
-
-// 		err := store.Store(events...)
-// 		require.NoError(t, err)
-// 	})
-
-// 	t.Run("Load", func(t *testing.T) {
-// 		events, err := store.Load("contact", contactId)
-// 		require.NoError(t, err)
-
-// 		assert.NotEmpty(t, events)
-// 	})
-// }
