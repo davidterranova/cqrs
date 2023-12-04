7056bbe663f7a963c8c1c9ac68768fb86bffc75e
david terranova <david@smallpdf.com>
2023-12-04 11:33:45 +0400

    system user

--- eventsourcing/user.go
+++ eventsourcing/user.go
@@ -1,6 +1,8 @@
 package eventsourcing
 
 import (
+	"errors"
+
 	"github.com/google/uuid"
 )
 
@@ -10,4 +12,23 @@ type User interface {
 	Id() uuid.UUID
 	String() string
 	FromString(string) error
+}
+
+var (
+	ErrInvalidUser = errors.New("invalid user")
+	SystemUser     = &systemUser{}
+
+	systemUserId = uuid.MustParse("99999999-9999-9999-9999-999999999999")
+)
+
+type systemUser struct{}
+
+func (u systemUser) Id() uuid.UUID  { return systemUserId }
+func (u systemUser) String() string { return "system" }
+func (u systemUser) FromString(s string) error {
+	if s != "system" {
+		return ErrInvalidUser
+	}
+
+	return nil
 }
